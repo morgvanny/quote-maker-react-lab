a68336996fd0b842510d9eaefcfff6d6c01e9d22
Morgan VanYperen <morgvanny@gmail.com>
2019-12-12 15:02:57 -0800

    fix signup flow

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -15,12 +15,13 @@ class UsersController < ApplicationController
 
   # POST /users
   def create
-    @user = User.new(user_params)
+    user = User.new(user_params)
 
-    if @user.save
-      render json: @user, status: :created, location: @user
+    if user.save
+      session[:user_id] = user.id
+      render json: user, status: :created, location: user
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: user.errors, status: :unprocessable_entity
     end
   end
 
@@ -46,6 +47,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:username, :password_digest)
+      params.require(:user).permit(:username, :password)
     end
 end
